e6e53437537638341da2a6f971439d35957c774f
Megha Arya <megha.arya07@gmail.com>
2019-12-03 14:23:48 -0600

    comment

--- MAAK/GameViewController.swift
+++ MAAK/GameViewController.swift
@@ -15,6 +15,8 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Testing github
+        
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
