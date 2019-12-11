51338e49dd9249321a1ea8912075e40434d63e79
Andrea Toba <andrea.i.toba@gmail.com>
2019-12-11 14:19:28 -0600

    hide hello

--- MAAK/GameScene.swift
+++ MAAK/GameScene.swift
@@ -252,10 +252,10 @@ class GameScene: SKScene {
         
         // Get label node from scene and store it for use later
        // self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
+        /*if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        }*/
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
